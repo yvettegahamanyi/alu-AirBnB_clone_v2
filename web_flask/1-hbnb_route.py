d6262702121d0303f9a0927f1eb92f6f75b56886
YvetteGahamanyi <ygahamanyi26@gmail.com>
2024-10-12 08:18:59 +0200

    ft: hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A Flask web application
+
+the application listen on 0.0.0.0, port 5000
+Routes:
+    /: display 'Hello HBNB!'
+    /hbnb: display 'HBNB'"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display 'HBNB'"""
